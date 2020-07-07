993c0ac3fd030e058fc93b638ae36b78b5377444
Rehman Ali <rehmanali.9442289@gmail.com>
2020-07-07 13:14:29 +0500

    Implement pie chart and dounghnut chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Doughnut } from 'react-chartjs-2'
+
+const data = {
+  labels: [
+    'Red',
+    'Green',
+    'Yellow'
+  ],
+  datasets: [{
+    data: [1],
+    backgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56',
+      '#AFCE56',
+      '#FFCEA6',
+      '#FAAE56'
+
+    ],
+    hoverBackgroundColor: [
+      '#FF6387',
+      '#36A2EC',
+      '#FFCE58'
+    ]
+  }]
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Doughnut data={data} />
     </div>
   );
 }
